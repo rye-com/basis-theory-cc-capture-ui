f6dbcde1690d3cadd68ef3226ecc1a0749941599
Andrew Parmar <andrew@rye.com>
2025-08-21 16:08:36 -0700

    Added basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,48 @@
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import {
   BasisTheoryProvider,
-  // CardElement,
   useBasisTheory,
   CardNumberElement,
   CardExpirationDateElement,
   CardVerificationCodeElement,
 } from "@basis-theory/react-elements";
 
+const inputStyle = {
+  base: {
+    // Default style
+    color: "#32325d",
+    fontFamily: '"Helvetica Neue", sans-serif',
+    fontSize: "16px",
+    ":focus": {
+      // Style when input has focus
+      color: "#80bdff",
+    },
+    padding: "10px",
+  },
+  invalid: {
+    // Style when input is invalid
+    color: "#fa755a",
+  },
+  complete: {
+    // Style when input is complete
+    color: "#4CAF50",
+  },
+  empty: {
+    // Style when input is empty
+    color: "#c4c4c4",
+  },
+}
+
 export default function App() {
   const { bt } = useBasisTheory("key_test_us_pub_Gtquo4kCeDkj2hTFWJSYCX");
-  // Ref to get access to the Element instance
-  const cardElementRef = useRef(null);
 
   // Refs to get access to the Elements instance
   const cardNumberRef = useRef(null);
   const cardExpirationRef = useRef(null);
   const cardVerificationRef = useRef(null);
-  
+
   // stores the current card brand in state, to pass to CardVerificationCodeElement
   const [cardBrand, setCardBrand] = useState();
-
-  // const submit = async () => {
-  //   try {
-  //     const token = await bt?.tokens.create({
-  //       type: 'card',
-  //       data: cardElementRef.current,
-  //     });
-  //     // store token.id in your database
-  //     console.log(token);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
 
   const submit = async () => {
     try {
@@ -45,73 +55,106 @@ export default function App() {
       if (!cardVerificationRef.current) {
         throw new Error("Card verification code is required");
       }
-      
+
       const token = await bt?.tokens.create({
-        type: 'card',
+        type: "card",
         data: {
           number: cardNumberRef.current,
           expiration_month: cardExpirationRef.current.month(),
           expiration_year: cardExpirationRef.current.year(),
           cvc: cardVerificationRef.current,
-        }
+        },
       });
       // TODO post the intent object to your backend
       console.log("Token id: ", token?.id);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   return (
     <>
       <div
         style={{
-          maxWidth: "500px",
+          maxWidth: "800px",
           margin: "50px auto",
           padding: "20px",
           justifyContent: "center",
           display: "flex",
           flexDirection: "column",
+          backgroundColor: "white",
+          borderRadius: "10px",
+          border: "1px solid #ccc",
         }}
       >
-        <h1 className="text-lg font-semibold mb-4 text-gray-800">
-          Enter Card Details
+        <h1
+          className="text-lg font-semibold mb-4 text-gray-800"
+          style={{ color: "black" }}
+        >
+          Enter Payment Info
         </h1>
-        {/* <BasisTheoryProvider bt={bt}>
-          <div style={{ marginBottom: "20px" }}>
-            <CardElement 
-              id="myCard" 
-              ref={cardElementRef} 
+
+        <BasisTheoryProvider bt={bt}>
+          <div
+            style={{
+              marginBottom: "2px",
+              border: "2px solid #ccc",
+              borderRadius: "5px",
+              padding: "5px",
+              backgroundColor: "white",
+              marginTop: "20px",
+            }}
+          >
+            <CardNumberElement
+              id="myCardNumber"
+              ref={cardNumberRef}
+              onChange={({ cardBrand }) => setCardBrand(cardBrand)}
+              style={inputStyle}
             />
           </div>
-
-          <button onClick={submit}>Submit</button>
-        </BasisTheoryProvider> */}
-        <BasisTheoryProvider bt={bt}>
-          <CardNumberElement
-            id="myCardNumber"
-            ref={cardNumberRef}
-            onChange={({ cardBrand }) => setCardBrand(cardBrand)}
-          />
-          <div style={{ display: 'flex' }}>
-            <div style={{ width: "100%" }}>
+          <div style={{ display: "flex", marginBottom: "10px" }}>
+            <div
+              style={{
+                width: "100%",
+                border: "2px solid #ccc",
+                marginRight: "1px",
+                borderRadius: "5px",
+              }}
+            >
               <CardExpirationDateElement
                 id="myCardExpiration"
                 ref={cardExpirationRef}
+                style={inputStyle}
               />
             </div>
-            <div style={{ width: "100%" }}>
+            <div
+              style={{
+                width: "100%",
+                border: "2px solid #ccc",
+                marginLeft: "1px",
+                borderRadius: "5px",
+              }}
+            >
               <CardVerificationCodeElement
                 id="myCardVerification"
                 ref={cardVerificationRef}
                 cardBrand={cardBrand}
+                style={inputStyle}
               />
             </div>
           </div>
-          <button onClick={submit}>Submit</button>
+          <button
+            onClick={submit}
+            style={{
+              borderRadius: "5px",
+              backgroundColor: "#2348FC",
+              color: "white",
+            }}
+          >
+            Submit
+          </button>
         </BasisTheoryProvider>
       </div>
     </>
-
   );
 }
